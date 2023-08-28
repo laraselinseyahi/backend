fcea5c283c7cb3de6f1027c426dc1145b16a5232
laraselinseyahi <107998503+laraselinseyahi@users.noreply.github.com>
2023-08-28 13:33:31 +0300

    swarms for cyto1

--- app.py
+++ app.py
@@ -808,6 +808,7 @@ def process_datavis():
     fig_cyto.update_layout(barmode='group', title={'text': "Characterization: Potency(IFNg and Cytotox)", 'font': {'size': 24,'color': 'blue'}, 'x': 0.5})
     #fig_cyto.show()
 
+    """"
     fig_cytokine_swarm1 = make_subplots(rows=1, cols=2, subplot_titles=("Cytokine", "Cytotoxicity" ))
 
     fig_cytokine_swarm1.add_trace(go.Box(y=CD19P_5_1, name="CD19+ 5:1", showlegend=False), row=1, col=1)
@@ -827,7 +828,7 @@ def process_datavis():
 
     fig_cytokine_swarm1.update_traces(boxpoints='all', jitter=0.5)
     fig_cytokine_swarm1.show()
-
+    """
     df_1 = dfs["Data Date Tracking"]
     patient_names = df_1.columns[1:].tolist()
     release_assays = []
@@ -1040,7 +1041,7 @@ def process_datavis():
         f.write(fig_memdiff_swarm2.to_html(full_html=False, include_plotlyjs='cdn'))
         f.write(fig_memdiff_swarm3.to_html(full_html=False, include_plotlyjs='cdn'))
         f.write(fig_cyto.to_html(full_html=False, include_plotlyjs='cdn'))
-        f.write(fig_cytokine_swarm1.to_html(full_html=False, include_plotlyjs='cdn'))
+        # f.write(fig_cytokine_swarm1.to_html(full_html=False, include_plotlyjs='cdn'))
        # f.write(fig_date.to_html(full_html=False, include_plotlyjs='cdn'))
         f.write(fig.to_html(full_html=False, include_plotlyjs='cdn'))
         f.write(fig_tbnk_table.to_html(full_html=False, include_plotlyjs='cdn'))
